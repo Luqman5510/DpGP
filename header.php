cef58f78d9b64307eaddbe465dc2b48aaefaf23f
Qurayshkhan <149657415+HassanKhanStackupSolution@users.noreply.github.com>
2024-09-29 23:21:17 +0500

    change:resolve color

--- header.php
+++ header.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DPPG Header</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <!-- Top Header -->
     <div class="top-header py-2">
@@ -43,47 +45,47 @@
         </div>
     </div>
     <!-- Bottom Header -->
-<div class="bottom-header py-3">
-    <div class="container">
-        <div class="row align-items-center">
-            <!-- Logo Section -->
-            <div class="col-md-3">
-                <a href="index.php" class="navbar-brand">
-                    <img src="image/logo.png" alt="Logo" class="img-fluid logo-img">
-                </a>
-            </div>
+    <div class="bottom-header py-3">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Logo Section -->
+                <div class="col-md-3">
+                    <a href="index.php" class="navbar-brand">
+                        <img src="image/logo.png" alt="Logo" class="img-fluid logo-img">
+                    </a>
+                </div>
 
-            <!-- Navigation Section -->
-            <div class="col-md-9">
-                <nav class="navbar navbar-expand-md justify-content-end">
-                    <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="portfolio.php">Portfolio</a>
-                            </li>
-                            <!-- <li class="nav-item">
+                <!-- Navigation Section -->
+                <div class="col-md-9">
+                    <nav class="navbar navbar-expand-md justify-content-end">
+                        <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="index.php">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="portfolio.php">Portfolio</a>
+                                </li>
+                                <!-- <li class="nav-item">
                                 <a class="nav-link" href="#">Services</a> -->
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="contact.php">Contact</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="about.php">About us</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="register.php">Get Started</a>
-                            </li>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="contact.php">Contact</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="about.php">About us</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="register.php">Get Started</a>
+                                </li>
 
-                        </ul>
-                    </div>
-                </nav>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
             </div>
         </div>
     </div>
-</div>
